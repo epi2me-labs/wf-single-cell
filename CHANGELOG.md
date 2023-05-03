7cb3ac44a2198bef236d50b8704d26a56557b1b6
Neil Horner <neil.horner@nanoporetech.com>
2023-05-03 11:04:32 +0000

    V0.2.3 bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.2.3]
+### Fixed 
+- Barcode assignment error when chromosome has no no data
+### Changed
+- Include reads in gene expression matrices (but not transcript matrices) that map to intron-only regions
+
 ## [v0.2.2]
 ### Fixed
 - Incorrect UMAP colors
--- bin/workflow_glue/check_sample_sheet.py
+++ bin/workflow_glue/check_sample_sheet.py
@@ -1,43 +1,93 @@
-"""Script to check that sample sheet is well-formatted."""
+"""Check if a sample sheet is valid."""
+import csv
 import sys
-
-import pandas as pd
 
 from .util import get_named_logger, wf_parser  # noqa: ABS101
 
 
 def main(args):
-    """Run entry point."""
-    logger = get_named_logger("check-sheet")
+    """Run the entry point."""
+    logger = get_named_logger("checkSheet")
+
+    barcodes = []
+    aliases = []
+    sample_types = []
+    allowed_sample_types = [
+        "test_sample", "positive_control", "negative_control", "no_template_control"
+        ]
 
     try:
-        logger.info(f"Reading {args.sample_sheet}.")
-        samples = pd.read_csv(args.sample_sheet, sep=None)
-        if 'alias' in samples.columns:
-            if 'sample_id' in samples.columns:
-                sys.stderr.write(
-                    "Warning: sample sheet contains both 'alias' and "
-                    'sample_id, using the former.')
-            samples['sample_id'] = samples['alias']
-        if not set(['sample_id', 'barcode']).intersection(samples.columns):
-            raise IOError()
-    except Exception:
-        raise IOError(
-            "Could not parse sample sheet, it must contain two columns "
-            "named 'barcode' and 'sample_id' or 'alias'.")
-    # check duplicates
-    dup_bc = samples['barcode'].duplicated()
-    dup_sample = samples['sample_id'].duplicated()
-    if any(dup_bc) or any(dup_sample):
-        raise IOError(
-            "Sample sheet contains duplicate values.")
-    samples.to_csv(args.output, sep=",", index=False)
-    logger.info(f"Written cleaned-up sheet to {args.output}.")
+        with open(args.sample_sheet, "r") as f:
+            csv_reader = csv.DictReader(f)
+            n_row = 0
+            for row in csv_reader:
+                n_row += 1
+                if n_row == 1:
+                    n_cols = len(row)
+                else:
+                    # check we got the same number of fields
+                    if len(row) != n_cols:
+                        raise ValueError(
+                            f"Unexpected number of cells in row number {n_row}."
+                        )
+                try:
+                    barcodes.append(row["barcode"])
+                except KeyError:
+                    sys.stdout.write("'barcode' column missing")
+                    sys.exit()
+                try:
+                    aliases.append(row["alias"])
+                except KeyError:
+                    sys.stdout.write("'alias' column missing")
+                    sys.exit()
+                try:
+                    sample_types.append(row["type"])
+                except KeyError:
+                    pass
+    except Exception as e:
+        sys.stdout.write(f"Parsing error: {e}")
+        sys.exit()
+
+    # check barcode and alias values are unique
+    if len(barcodes) > len(set(barcodes)):
+        sys.stdout.write("values in 'barcode' column not unique")
+        sys.exit()
+    if len(aliases) > len(set(aliases)):
+        sys.stdout.write("values in 'alias' column not unique")
+        sys.exit()
+
+    if sample_types:
+        # check if "type" column has unexpected values
+        unexp_type_vals = set(sample_types) - set(allowed_sample_types)
+
+        if unexp_type_vals:
+            sys.stdout.write(
+                f"found unexpected values in 'type' column: {unexp_type_vals}. "
+                f"Allowed values are: {allowed_sample_types}"
+            )
+            sys.exit()
+
+        if args.required_sample_types:
+            for required_type in args.required_sample_types:
+                if required_type not in allowed_sample_types:
+                    sys.stdout.write(f"Not an allowed sample type: {required_type}")
+                    sys.exit()
+                if sample_types.count(required_type) < 1:
+                    sys.stdout.write(
+                        f"Sample sheet requires at least 1 of {required_type}")
+                    sys.exit()
+
+    logger.info(f"Checked sample sheet {args.sample_sheet}.")
 
 
 def argparser():
     """Argument parser for entrypoint."""
-    parser = wf_parser("check-sample-sheet")
-    parser.add_argument('sample_sheet')
-    parser.add_argument('output')
+    parser = wf_parser("check_sample_sheet")
+    parser.add_argument("sample_sheet", help="Sample sheet to check")
+    parser.add_argument(
+        "--required_sample_types",
+        help="List of required sample types. Each sample type provided must "
+             "appear at least once in the sample sheet",
+        nargs="*"
+    )
     return parser
