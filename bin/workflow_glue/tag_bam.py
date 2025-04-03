742eafeb8f1a0165c9b4303e8e01d8bc72e872fe
Neil Horner <neil.horner@nanoporetech.com>
2025-04-03 12:17:43 +0000

    CW-5649 Supplementary records missing

--- bin/workflow_glue/tag_bam.py
+++ bin/workflow_glue/tag_bam.py
@@ -32,7 +32,8 @@ def argparser():
 
     parser.add_argument(
         "out_bam", type=Path,
-        help="Path for tagged output BAM")
+        help="Path for the tagged output BAM",
+        default='-')
 
     parser.add_argument(
         "tags", type=Path,
@@ -96,7 +97,7 @@ class TagStore:
                     chrom = getattr(next(iter(d.values())), "chrom")
                     self._index[chrom] = fname
                 except StopIteration:
-                    logger.warn(f"{fname} appears empty.")
+                    logger.warning(f"{fname} appears empty.")
                 else:
                     logger.info(f"{fname} contains tags for reference: {chrom}.")
         else:
@@ -107,7 +108,10 @@ class TagStore:
         """Populate the proxy for a given reference."""
         if not self._single:
             self._cur = rname
-            self._tags = self._read_file(self._index[self._cur])
+            try:
+                self._tags = self._read_file(self._index[self._cur])
+            except KeyError:
+                self._tags = {}
 
     def _read_file(self, fname, nrows=None, cols=None):
         """Read a tags file."""
@@ -154,42 +158,82 @@ def add_tags(tags, in_bam, out_bam, threads):
     """Add all the required tags to the BAM file."""
     store = TagStore(tags, bam=in_bam)
 
-    skipped = 0
-    written = 0
-    with pysam.AlignmentFile(in_bam, "rb", threads=threads) as bam_in:
-        with pysam.AlignmentFile(
-                out_bam, "wb", template=bam_in, threads=threads) as bam_out:
-            for ref in bam_in.references:
-                logger.info(f"Processing reads from reference: {ref}.")
-                try:
-                    store.populate(ref)
-                except KeyError:
-                    logger.warn(f"Could not find tag data for reference: {ref}.")
+    n_skipped = 0
+    n_prim_written = 0
+    n_supp_written = 0
+
+    threads = max(1, threads // 2)
+
+    b1 = pysam.AlignmentFile(in_bam, "rb", threads=threads)
+    # Uncompressed BAM if writing to stdout
+    write_mode = 'wbu' if out_bam == "-" else 'wb'
+    b2 = pysam.AlignmentFile(out_bam, write_mode, template=b1, threads=threads)
+
+    sup_tags = {}
+
+    with b1 as bam_in, b2 as bam_out:
+
+        for ref in bam_in.references:
+            logger.info(f"Processing reads from reference: {ref}.")
+            store.populate(ref)
+
+            logger.info("Tagging reads.")
+            for align in bam_in.fetch(ref):
+                read_id = align.query_name
+
+                if align.is_supplementary:
+                    # No tagging of supp. reads yet, that's done later
                     continue
-                logger.info("Tagging reads.")
-                for align in bam_in.fetch(ref):
-                    read_id = align.query_name
-                    try:
-                        row = store._tags[read_id]
-                    except KeyError:
-                        skipped += 1
-                        continue  # don't write reads without tags
-                    else:
-                        written += 1
+                try:
+                    row = store._tags[read_id]
+                except KeyError:
+                    # No tags for this read
+                    n_skipped += 1
+                    continue
+
+                # If primary has tags and a supp record, save the ID and tags
+                # for later tagging.
+                try:
+                    if align.get_tag("SA"):
+                        tags = []
                         for tag in BAM_TAGS.values():
-                            align.set_tag(tag, getattr(row, tag), value_type="Z")
-                        bam_out.write(align)
-    total = skipped + written
-    written_pct = 0
-    skipped_pct = 0
-    if total > 0:
-        written_pct = 100 * written / total
-        skipped_pct = 100 * skipped / total
-    logger.info(
-        f"Written: {written} ({written_pct:0.2f}%). "
-        f"Skipped: {skipped} ({skipped_pct:0.2f}%).")
+                            tags.append(getattr(row, tag))
+                        sup_tags[read_id] = tags
+                except KeyError:
+                    pass  # No SA tag
+                n_prim_written += 1
+                for tag in BAM_TAGS.values():
+                    align.set_tag(tag, getattr(row, tag), value_type="Z")
+                bam_out.write(align)
+
+        bam_in.reset()  # reset the iterator, this time tagging supp. records
+        for record in bam_in.fetch():
+            if not record.is_supplementary:
+                continue
+            read_id = record.query_name
+            try:
+                tags = sup_tags[read_id]
+            except KeyError:
+                # No tags for this read
+                continue
+            for tag, val in zip(BAM_TAGS.values(), tags):
+                record.set_tag(tag, val, value_type="Z")
+            n_supp_written += 1
+            bam_out.write(record)
+
+        total = n_skipped + n_prim_written
+        written_pct = 0
+        skipped_pct = 0
+        if total > 0:
+            written_pct = 100 * n_prim_written / total
+            skipped_pct = 100 * n_skipped / total
+        logger.info(
+            f"Tagged primary records: {n_prim_written} ({written_pct:0.2f}%).\n"
+            f"Skipped primary records: {n_skipped} ({skipped_pct:0.2f}%).\n"
+            f"Tagged suppl. records: {n_supp_written}.")
 
 
 def main(args):
     """Entry point."""
-    add_tags(args.tags, args.in_bam, args.out_bam, args.threads)
+    add_tags(
+        args.tags, args.in_bam, args.out_bam, args.threads)
--- bin/workflow_glue/tests/test_tag_bam.py
+++ bin/workflow_glue/tests/test_tag_bam.py
@@ -9,92 +9,104 @@ import pytest
 from workflow_glue import tag_bam
 
 
-@pytest.fixture
-def read():
-    """Get a randomly-generated gene sequence."""
-    return (
+def make_bam(bam_entries):
+    """Make a BAM file."""
+    read = (
         "ATTCAGCGCTGGAGACCGAGCGCCCCGCAAAGGGCCTGATCT"
         "ATCGCGCACGGGACTACTCATTGGGACTGCGGCAATAGGGGAGGGGCCTAACAACGTT")
+    chrs = set([x[1] for x in bam_entries])
 
+    # Create the BAM file to be tagged
+    header = '\n'.join([f'@SQ	SN:{chr_}	LN:10000000' for chr_ in chrs])
 
-@pytest.fixture
-def input_bam(read):
-    """Create a dummy bam file.
-
-    Each entry has the same the same random sequence but a unique read_id.
-    """
-    header = """@SQ	SN:chr17	LN:10000000"""
-    read_ids = ['read1', 'read2']
     entries = [f'{header}']
 
-    for read_id in read_ids:
+    for records in bam_entries:
         # Make a sam file containing the read and a quality qscore of 60.
-        entries.append((
-            f"{read_id}\t0\tchr17\t1\t60\t{len(read)}M\t*\t0\t0\t"
-            f"{read}\t{'?' * len(read)}"
-        ))
+        id_, chr_, flag, sa_tag = records
+        entries.append(
+            f"{id_}\t{flag}\t{chr_}\t1\t60\t{len(read)}M\t*\t0\t0\t"
+            f"{read}\t{'?' * len(read)}\t{sa_tag}"
+        )
     sam = '\n'.join(entries)
     # Write out a test BAM
     with tempfile.NamedTemporaryFile(mode='w', suffix='.sam', delete=False) as fh_sam:
         fh_sam.write(sam)
         sam_file = fh_sam.name
 
-    bam = tempfile.NamedTemporaryFile('w', delete=False, suffix='.bam').name
-    sub.check_output(['samtools', 'view', sam_file, '-o', bam])
-    sub.check_output(['samtools', 'index', bam])
+    test_bam = tempfile.NamedTemporaryFile('w', delete=False, suffix='.bam').name
+    sub.check_output(['samtools', 'view', sam_file, '-o', test_bam])
+    sub.check_output(['samtools', 'index', test_bam])
+    return test_bam
 
-    return bam
 
+@pytest.mark.parametrize(
+    "tags,bam_entries",
+    [
+        (  # A single entry from a tag file representing a primary alignment.
+            {
+                'read_id': 'read1',
+                'CR': 'AAAAAAAAAAAAAgAA',
+                'CB': 'AAAAAAAAAAAAAaAA',
+                'CY': '????????????????',
+                'UR': 'GGGGGtGGGGGG',
+                'UB': 'GGGGGGGGGGGG',
+                'UY': '????????????',
+                'GN': 'YFG',
+                'TR': 'YFT',
+                'chr': 'chr1',
+                'start': 1000,
+                'end': 2000
 
-@pytest.fixture
-def tags_file():
-    """Create a tags TSV file."""
-    tags_header = (
-        'read_id', 'CR', 'CB', 'CY', 'UR', 'UB', 'UY', 'chr', 'start', 'end', 'gene',
-        'transcript')
-    tags_rows = (
-        ('read1', 'AAAAAAAAAAAAAgAA', 'AAAAAAAAAAAAAaAA', '????????????????',
-         'GGGGGtGGGGGG', 'GGGGGGGGGGGG', '????????????', 'chr17', 1000, 2000,
-         'YFG', 'YFT'),
-        ('read2', 'TcTTTTTTTTTTTTTT', 'TTTTTTTTTTTTTTTT', '****************',
-         'CCCCCCaCCCCC', 'CCCCCCCCCCCC', '????????????', 'chr17', 4000, 5000,
-         'YFG1', 'YFT2')
+            },
+            # Two alignment records one primary and one supplementary.
+            (
+                ('read1', 'chr1', 0, "SA:Z:chr2,10000,+,10S100M1S,60,11"),
+                ('read1', 'chr2', 2048, "")
+            )
+         )
+    ]
+)
+def test_add_tags(tags, bam_entries):
+    """Check that the output - bams and tag file - are correct."""
+    bam_out = tempfile.NamedTemporaryFile('w', suffix='.bam', delete=False).name
 
-    )
+    tags_to_test = ['CR', 'CB', 'CY', 'UR', 'UB', 'UY', 'GN', 'TR']
+    # Create the tags file
+    tags_header = tags.keys()
+    tags_rows = [tags.values()]
+
     tags_df = (
         pd.DataFrame(tags_rows, columns=tags_header)
         .set_index('read_id', drop=True)
-        .rename(
+        .rename(  # Convert to long tag form - those expected in the tags file
             columns={v: k for k, v in tag_bam.BAM_TAGS.items()})
     )
-    tags = tempfile.NamedTemporaryFile(mode='w', suffix='.tsv', delete=False)
-    tags_df.to_csv(tags.name, sep='\t')
 
-    return tags.name
+    tags_file = tempfile.NamedTemporaryFile(mode='w', suffix='.tsv', delete=False)
+    tags_df.to_csv(tags_file.name, sep='\t')
 
+    test_bam = make_bam(bam_entries)
 
-def test_add_tags(tags_file, input_bam):
-    """Check that the output - bams and tag file - are correct."""
-    # create_inputs
-    out_bam = tempfile.NamedTemporaryFile('w', suffix='.bam').name
-    tag_bam.add_tags(Path(tags_file), input_bam, out_bam, 1)
+    # Run the test
+    tag_bam.add_tags(
+        Path(tags_file.name), test_bam, bam_out, threads=1)
 
-    # Check that the correct tags have been set
-    with pysam.AlignmentFile(out_bam, "rb") as bam_result:
+    # Check that the correct tags have been set on primary and supplementary
+    # record.
+    tagged = 0
+    with pysam.AlignmentFile(bam_out, "rb") as bam_result:
         for align in bam_result:
-            read_id = align.query_name
-            # Check that some of the tags have been set correctly
-            if read_id == 'read1':
-                assert align.get_tag('CR') == 'AAAAAAAAAAAAAgAA'
-                assert align.get_tag('CB') == 'AAAAAAAAAAAAAaAA'
-                assert align.get_tag('CY') == '????????????????'
-                assert align.get_tag('UR') == 'GGGGGtGGGGGG'
-                assert align.get_tag('UB') == 'GGGGGGGGGGGG'
-                assert align.get_tag('UY') == '????????????'
+            for expected_tag, expected_value in tags.items():
+                if expected_tag in tags_to_test:
+                    assert align.get_tag(expected_tag) == expected_value
+            tagged += 1
+    assert tagged == 2
 
 
-def test_empty_file(input_bam):
+def test_empty_file():
     """Test giving a header-only tags file, in a tags directory, to tag_bams."""
+    input_bam = make_bam([('read1', 'chr1', 0, '')])
     tags_header = (
         'read_id', 'CR', 'CB', 'CY', 'UR', 'UB', 'UY', 'chr', 'start', 'end', 'gene',
         'transcript')
@@ -108,5 +120,5 @@ def test_empty_file(input_bam):
         tmp_test_dir = Path(fh)
         header_only_file = tmp_test_dir / 'test_tags.tsv'
         tags_df.to_csv(header_only_file, sep='\t')
-        out_bam = tempfile.NamedTemporaryFile('w', suffix='.bam', delete=False).name
-        tag_bam.add_tags(tmp_test_dir, input_bam, out_bam, 1)
+        out_bam_p = tempfile.NamedTemporaryFile('w', suffix='.bam', delete=False).name
+        tag_bam.add_tags(tmp_test_dir, input_bam, out_bam_p, threads=1)
